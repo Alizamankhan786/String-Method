38c33fe465d44cb59addf2e2e3ebd01b090167bc
Alizamankhan786 <az030366@gmail.com>
2024-04-29 11:33:44 -0700

    index file created

---
+++ app.js
@@ -0,0 +1,86 @@
+
+// QUESTION 1
+
+// const itemarray = [{
+//     name : `juice`, price: `50`, quantity : `3`}, {
+//     name : `cookie` , price : `30`, quantity: `9`},{
+//     name : `shirt` , price : `880`, quantity: `1`},{
+//     name : `pen` , price : `100` ,quantity: `2` },]
+
+
+//     console.log(50+30+880+100);
+//     console.log(3 + 9 + 1 +2);
+
+
+// QUESTION 2
+
+
+
+// const obj = {
+
+//     name : `Ali`,
+//     email : `az030366@gmail.com`,
+//     password : `password`,
+//     age : `20`,
+//     gender : `male`,
+//     country : `pakistan`,
+//     city : `karachi`
+    
+
+// }
+
+// console.log(obj);
+
+
+// obj.firstName = `usman`;
+// obj.lastName = `zaman`;
+
+
+
+// QUESTION 3
+
+
+// Constructor function
+// function Person(name, age, gender) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+  
+//   // Creating multiple records using the constructor
+// let person1 = new Person("Alice", 30, "Female");
+// let person2 = new Person("Bob", 25, "Male");
+// let person3 = new Person("Charlie", 35, "Male");
+  
+//   // Accessing properties of each record
+//   console.log("Person 1:", person1.name, person1.age, person1.gender);
+//   console.log("Person 2:", person2.name, person2.age, person2.gender);
+//   console.log("Person 3:", person3.name, person3.age, person3.gender);
+
+
+// Question 4
+
+// incomplete
+
+
+//  function Person(name, address, gender, education , profession,) {
+//     this.name = name;
+//     this.address = address;
+//     this.gender = gender;
+//     this.education = education;
+//     this.profession = profession;
+// }
+
+
+// let person1 = new Person("ali", `az366@gmail.com`, "male" , "intermediate" , "artist");
+// let person2 = new Person("usman", `usman366@gmail.com`, "male" , "intermediate" , "cricket");
+// let person3 = new Person("rayyan", `ryn366@gmail.com`, "male" , "intermediate" , "reading books");
+// let person4 = new Person("taha", `taha366@gmail.com`, "male" , "intermediate" , "writing");
+
+
+
+
+
+
+
+  
